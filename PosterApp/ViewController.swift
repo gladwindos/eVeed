9fb523971eabe07b45bd7988f8545b6cbcb13a86
gladwindos <gladwin.dos@hotmail.co.uk>
2015-12-10 19:12:28 +0000

    Initial Commit

---
+++ PosterApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PosterApp
+//
+//  Created by Gladwin Dosunmu on 10/12/2015.
+//  Copyright © 2015 Gladwin Dosunmu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
