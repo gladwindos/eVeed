28061ac50dd80b17aae3f994a4d39967712c5a56
gladwindos <gladwin.dos@hotmail.co.uk>
2016-02-03 19:56:26 +0000

    Fixed "Attempting to load the view of a view controller while it is deallocating is not allowed and may result in undefined behavior (<UISearchController:" issue

--- eVeed/SearchTableViewController.swift
+++ eVeed/SearchTableViewController.swift
@@ -131,12 +131,19 @@ class SearchTableViewController: UITableViewController {
         tableView.tableHeaderView = searchController.searchBar
         
         loadEvents()
+        
+        self.searchController.loadViewIfNeeded()
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+    
+    deinit {
+        
+        self.searchController.loadViewIfNeeded()
     }
 
     override func didReceiveMemoryWarning() {
